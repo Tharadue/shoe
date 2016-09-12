f97b75bdca17f7801312fa9b417005364275c215
Thara Due Jeppesen <thar0076@edu.eal.dk>
2016-09-12 09:33:22 +0200

    adding more shoes

--- demo-data.js
+++ demo-data.js
@@ -38,6 +38,46 @@ exports.SHOEDATA = [{
         "picture_three": "mens-timberland-westbank-3",
         "price": "199.00",
         "description": "This is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using"
+    },
+    {
+        "id": 5,
+        "name": "Westbank 6'' Boot WP",
+        "brand": "timberland",
+        "picture_one": "mens-timberland-westbank-1",
+        "picture_two": "mens-timberland-westbank-2",
+        "picture_three": "mens-timberland-westbank-3",
+        "price": "199.00",
+        "description": "This is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using"
+    },
+    {
+        "id": 6,
+        "name": "Nike ISO 3 - Turn it up",
+        "brand": "nike",
+        "picture_one": "mens-timberland-westbank-1",
+        "picture_two": "mens-timberland-westbank-2",
+        "picture_three": "mens-timberland-westbank-3",
+        "price": "199.00",
+        "description": "This is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using"
+    },
+    {
+        "id": 7,
+        "name": "Hurricane Space Fighter",
+        "brand": "saucony",
+        "picture_one": "mens-timberland-westbank-1",
+        "picture_two": "mens-timberland-westbank-2",
+        "picture_three": "mens-timberland-westbank-3",
+        "price": "199.00",
+        "description": "This is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using"
+    },
+    {
+        "id": 8,
+        "name": "Nike Nice Trainer, Do it",
+        "brand": "saucony",
+        "picture_one": "mens-timberland-westbank-1",
+        "picture_two": "mens-timberland-westbank-2",
+        "picture_three": "mens-timberland-westbank-3",
+        "price": "199.00",
+        "description": "This is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using"
     }
 ];
 //# sourceMappingURL=demo-data.js.map
